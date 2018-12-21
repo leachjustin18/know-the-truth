8978e3d76b0d68eb3faee498de87df2d02401965
leachjustin18 <leachjustin18@yahoo.com>
2018-12-20 21:11:43 -0600

    Added addtional page for testing

---
+++ src/pages/choose-to-overcome.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import Layout from '../layout';
+import './page.scss';
+
+class ChooseToOverCome extends React.PureComponent<{}, {}> {
+  public render() {
+    return (
+      <Layout>
+        <Helmet
+          title="Choose To Overcome"
+          meta={[
+            {
+              name: 'description',
+              content: 'Choose To Overcome',
+            },
+          ]}
+        />
+        <h1>Choose To Overcome</h1>
+
+        <div className="page__video">
+          <iframe
+            width="100%"
+            height="315"
+            src="https://www.youtube.com/embed/8np6315tffA?rel=0&autoplay=1"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen={true}
+          />
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export default ChooseToOverCome;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,19 @@ class IndexPage extends React.PureComponent<{}, {}> {
       <Layout>
         <h1>Welcome to Know the Truth series!</h1>
         <h2>Please click link below to see monthly lesson</h2>
-        <Link to="/what-must-I-do-to-be-saved/">
-          What Must I Do To Be Saved?
-        </Link>
+        <ol>
+          <li>
+            <strong>February 2018: </strong>
+            <Link to="/what-must-I-do-to-be-saved/">
+              What Must I Do To Be Saved?
+            </Link>
+          </li>
+
+          <li>
+            <strong>March 2018: </strong>
+            <Link to="/choose-to-overcome/">Choose To Overcome</Link>
+          </li>
+        </ol>
       </Layout>
     );
   }
